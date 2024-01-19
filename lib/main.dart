ea8fd92a4873f12642d98cd8d11daeb7dd054334
ammarsultan <ammar.sultan@edu.umi.ac.ma>
2024-01-19 10:51:43 +0100

    A late initial commit, now the project Magic 8 ball is at initial state after configuring the pupspec:yaml file and rendering the starter interface.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,37 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  const BallPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Ball();
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({super.key});
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      color: Colors.blue,
+      onPressed: () {
+        print('I got clicked');
+      },
+      child: const Image(
+        image: AssetImage('images/ball1.png'),
+      ),
+    );
+  }
+}
